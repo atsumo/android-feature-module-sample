d385cef45d4ed5535520adbbe9a4fe624f8e8754
atsumo <bachisuta@gmail.com>
2018-05-17 00:04:19 +0900

    first commit

---
+++ app/src/main/java/com/github/atsumo/android_feature_module_sample/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.github.atsumo.android_feature_module_sample.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+  // TODO: Implement the ViewModel
+}
